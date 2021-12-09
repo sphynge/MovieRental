0a32c72c8a4e0e32a22a4798e89fdea2cc622669
sphynge <sphynge@gmail.com>
2021-12-10 00:46:19 +0100

    Rename variables and delete comments on Customer

--- MovieRental/Customer.cs
+++ MovieRental/Customer.cs
@@ -2,10 +2,8 @@
 
 namespace MovieRental
 {
-
     public class Customer
     {
-
         private readonly string _name;
         private readonly List<Rental> _rentals;
 
@@ -27,49 +25,44 @@ namespace MovieRental
 
         public string Statement()
         {
-            double totalAmount = 0;
-            int frequentRenterPoints = 0;
-            string result = "\nRental Record for " + GetName() + "\n";
+            var totalAmount = 0.0;
+            var frequentRenterPoints = 0;
+            var result = "\nRental Record for " + GetName() + "\n";
 
-            foreach (Rental each in _rentals)
+            foreach (var rental in _rentals)
             {
                 double thisAmount = 0;
 
-                //determine amounts for each line
-                switch (each.Movie.PriceCode)
+                switch (rental.Movie.PriceCode)
                 {
                     case Movie.Regular:
                         thisAmount += 2;
-                        if (each.DaysRented > 2)
-                            thisAmount += (each.DaysRented - 2) * 1.5;
+                        if (rental.DaysRented > 2)
+                            thisAmount += (rental.DaysRented - 2) * 1.5;
                         break;
                     case Movie.NewRelease:
-                        thisAmount += each.DaysRented * 3;
+                        thisAmount += rental.DaysRented * 3;
                         break;
                     case Movie.Children:
                         thisAmount += 1.5;
-                        if (each.DaysRented > 3)
-                            thisAmount += (each.DaysRented - 3) * 1.5;
+                        if (rental.DaysRented > 3)
+                            thisAmount += (rental.DaysRented - 3) * 1.5;
                         break;
                 }
 
-                // add frequent renter points
                 frequentRenterPoints++;
-                // add bonus for a two day new release rental
-                if ((each.Movie.PriceCode == Movie.NewRelease) && each.DaysRented > 1)
+
+                if ((rental.Movie.PriceCode == Movie.NewRelease) && rental.DaysRented > 1)
                     frequentRenterPoints++;
 
-                // show figures for this rental
-                result += "\t" + each.Movie.Title + "\t" + thisAmount.ToString() + "\n";
+                result += "\t" + rental.Movie.Title + "\t" + thisAmount + "\n";
                 totalAmount += thisAmount;
             }
 
-            // add footer lines
-            result += "Amount owed is " + totalAmount.ToString() + "\n";
-            result += "You earned " + frequentRenterPoints.ToString() + " frequent renter points\n";
+            result += "Amount owed is " + totalAmount + "\n";
+            result += "You earned " + frequentRenterPoints + " frequent renter points\n";
 
             return result;
         }
     }
 }
-
