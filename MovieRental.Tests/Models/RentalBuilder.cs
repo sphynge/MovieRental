2435e242032d0ddc9f9ea0366d7be2f3781fa055
sphynge <sphynge@gmail.com>
2021-12-10 23:37:18 +0100

    Remove unused methods on RentalBuilder

--- MovieRental.Tests/Models/RentalBuilder.cs
+++ MovieRental.Tests/Models/RentalBuilder.cs
@@ -2,27 +2,8 @@
 
 namespace MovieRental.Tests.Models
 {
-    internal class RentalBuilder
+    internal static class RentalBuilder
     {
-        private Movie _movie;
-        private int _daysRented;
-
-        public RentalBuilder WithMovie(Movie movie)
-        {
-            this._movie = movie;
-            return this;
-        }
-
-        public RentalBuilder WithDaysRented(int daysRented)
-        {
-            this._daysRented = daysRented;
-            return this;
-        }
-
-        public Rental Build()
-        {
-            return new Rental(_movie, _daysRented);
-        }
 
         public static Rental Regular(int daysRented)
         {
