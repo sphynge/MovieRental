67b3088ddfbb4634d8c8cdbce9632a592bdb9c73
sphynge <sphynge@gmail.com>
2021-12-10 01:36:37 +0100

    Moved GetCharge to Movie

--- MovieRental/Rental.cs
+++ MovieRental/Rental.cs
@@ -13,28 +13,7 @@
 
         public double GetCharge()
         {
-            double result = 0;
-
-            switch (Movie.PriceCode)
-            {
-                case Movie.Regular:
-                    result += 2;
-                    if (DaysRented > 2)
-                        result += (DaysRented - 2) * 1.5;
-                    break;
-
-                case Movie.NewRelease:
-                    result += DaysRented * 3;
-                    break;
-
-                case Movie.Children:
-                    result += 1.5;
-                    if (DaysRented > 3)
-                        result += (DaysRented - 3) * 1.5;
-                    break;
-            }
-
-            return result;
+            return Movie.GetCharge(DaysRented);
         }
 
         public int GetFrequentRenterPoints()
